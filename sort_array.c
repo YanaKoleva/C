aef1cdb0c22af6a45c07f6f0e08de63aab0eab52
Yana <63018142+YanaKoleva@users.noreply.github.com>
2022-05-08 00:05:20 +0100

    Add files via upload

---
+++ sort_array.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	int i;
+	int j;
+
+	printf("Enter numer for array:\n");
+	scanf("%d", &n);
+	
+	int array[n];
+
+	for(i=0;i<n;i++)
+	{
+		printf("Enter number:\n");
+		scanf("%d", &array[i]);
+	}
+
+	for(j=0;j<n;j++)
+	{
+		for(i=0;i<n;i++)
+		{
+			if(array[j]<array[i])
+			{
+				array[i] = array[i] - array[j];
+				array[j] = array[i] + array[j];
+				array[i] = array[j] - array[i];
+			}
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("Number %d: %d\n", i, array[i]);
+	}
+	
+	return 0;
+}
